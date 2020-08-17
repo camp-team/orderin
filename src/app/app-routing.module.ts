84ca36844dcfb2b3fad9ae318d30907cc1dab11b
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 02:40:51 +0900

    feat(about): create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,14 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
   },
   {
+    path: 'cuisines',
+    loadChildren: () => import('./cuisines/cuisines.module').then((m) => m.CuisinesModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then((m) => m.AboutModule)
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then((m) => m.LoginModule)
   },
