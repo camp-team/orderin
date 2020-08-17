bd2386ed2a992960d0b78283cb2a6ebc70341a4b
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 01:22:23 +0900

    feat(angularfire): init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBN09WPmAROh0H9LK_hNV4-a81ud-KG_QI',
+    authDomain: 'orderin-9b18a.firebaseapp.com',
+    databaseURL: 'https://orderin-9b18a.firebaseio.com',
+    projectId: 'orderin-9b18a',
+    storageBucket: 'orderin-9b18a.appspot.com',
+    messagingSenderId: '542314710655',
+    appId: '1:542314710655:web:26770000870b7c3a9c214a',
+    measurementId: 'G-J1QZSL7VXB'
+  }
 };
 
 /*
