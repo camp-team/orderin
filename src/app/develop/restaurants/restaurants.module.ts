951d3c81dab92aabe18eb9073f2a3168d2105a87
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-19 02:12:28 +0900

    feat(restaurants): add grid

--- src/app/develop/restaurants/restaurants.module.ts
+++ src/app/develop/restaurants/restaurants.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { RestaurantsRoutingModule } from './restaurants-routing.module';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { SharedModule } from 'src/app/develop/shared/shared.module';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 @NgModule({
   declarations: [RestaurantsComponent],
   imports: [
     CommonModule,
-    RestaurantsRoutingModule
+    RestaurantsRoutingModule,
+    SharedModule,
+    MatCheckboxModule
   ]
 })
 export class RestaurantsModule { }
--- src/app/develop/restaurants/restaurants/restaurants.component.ts
+++ src/app/develop/restaurants/restaurants/restaurants.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantGetService } from 'src/app/services/restaurant-get.service';
+import { Restaurant } from 'src/app/interfaces/restaurant';
 
 @Component({
   selector: 'app-restaurants',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+  restaurants: Restaurant[];
+
+  cuisines = [
+    'Indian',
+    'Korean',
+    'Chinese',
+    'Western',
+    'Tibetan',
+    'Cafe',
+    'Bakery',
+    'Dessert',
+    'Italian',
+    'Momos',
+    'Fast Food',
+    'Thai'
+  ];
+
+
+  constructor(
+    private restaurantGetService: RestaurantGetService
+  ) {
+    this.restaurantGetService.getRestaurants().subscribe((restaurants: Restaurant[]) => {
+      this.restaurants = restaurants;
+    });
+  }
 
   ngOnInit(): void {
   }
