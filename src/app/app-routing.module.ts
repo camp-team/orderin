24224eca0064a5021e4177b3c35f45b6926f1e70
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 14:03:53 +0900

    feat(app): delete content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
-  },
-  {
-    path: 'cuisines',
-    loadChildren: () => import('./cuisines/cuisines.module').then((m) => m.CuisinesModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then((m) => m.AboutModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then((m) => m.LoginModule)
-  },
+    loadChildren: () => import('./shell/shell.module').then((m) => m.ShellModule)
+  }
 ];
 
 @NgModule({
