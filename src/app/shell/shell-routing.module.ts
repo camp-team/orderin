36806733d06f798e6910286b93532e69b7781c7c
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 14:04:06 +0900

    feat(shell): update routing

--- src/app/shell/shell-routing.module.ts
+++ src/app/shell/shell-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+      },
+      {
+        path: 'cuisines',
+        loadChildren: () => import('./cuisines/cuisines.module').then((m) => m.CuisinesModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('./about/about.module').then((m) => m.AboutModule)
+      }
+    ]
+  },
+  {
+    path: 'develop',
+    loadChildren: () => import('../develop/develop.module').then((m) => m.DevelopModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ShellRoutingModule } from './shell-routing.module';
 import { ShellComponent } from './shell/shell.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [ShellComponent],
   imports: [
     CommonModule,
-    ShellRoutingModule
+    ShellRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class ShellModule { }
