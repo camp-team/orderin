f52d187999b723506a83c950722e17fcbdb695bf
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 14:03:10 +0900

    feat(home): create

---
+++ src/app/shell/home/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  cuisines = [
+    'Indian',
+    'Korean',
+    'Chinese',
+    'Western',
+    'Tibetan',
+    'Cafe',
+    'Bakery',
+    'Dessert',
+    'Italian',
+    'Momos',
+    'Fast Food',
+    'Thai'
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
