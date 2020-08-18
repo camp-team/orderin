9ab839c26bca4dbb8d3b962ade341017a017e30c
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 17:04:47 +0900

    feat(shared): create

---
+++ src/app/develop/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
+
+
+@NgModule({
+  declarations: [RestaurantCardComponent],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ]
+})
+export class SharedModule { }
