439164fd74fefdc6b5695b5c4b4902014fac4ae8
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 12:22:59 +0900

    feat(home): add filters

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  cuisines = [
+    'Indian',
+    'Korean',
+    'Chinese',
+    'Western',
+    'Tibetan',
+    'Cafe',
+    'Bakery',
+    'Dessert',
+    'Italian',
+    'Momos',
+    'Fast Food',
+    'Thai'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
