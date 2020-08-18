63f9c9177d86bd3dc09096b3c2c3d683e808db1e
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-18 14:27:01 +0900

    feat(develop): add sidenav

--- src/app/develop/develop.module.ts
+++ src/app/develop/develop.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { DevelopRoutingModule } from './develop-routing.module';
 import { DevelopComponent } from './develop/develop.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [DevelopComponent],
   imports: [
     CommonModule,
-    DevelopRoutingModule
+    DevelopRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ]
 })
 export class DevelopModule { }
