9624724f40ae59815305050079ae731501f1151c
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-19 02:12:41 +0900

    feat(restauant card): add chips

--- src/app/develop/shared/shared.module.ts
+++ src/app/develop/shared/shared.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 
 
 @NgModule({
   declarations: [RestaurantCardComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MatButtonModule,
+    MatChipsModule
+  ],
+  exports: [
+    RestaurantCardComponent
   ]
 })
 export class SharedModule { }
