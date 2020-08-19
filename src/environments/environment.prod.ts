6a9d3eadccaba980688b1545e46372836e1ebf59
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-19 22:24:18 +0900

    feat(environment): add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCng_R5yDG35I9iSmqJhJYhuLyNBRYwhQ4',
+    authDomain: 'orderin-prod.firebaseapp.com',
+    databaseURL: 'https://orderin-prod.firebaseio.com',
+    projectId: 'orderin-prod',
+    storageBucket: 'orderin-prod.appspot.com',
+    messagingSenderId: '893122062399',
+    appId: '1:893122062399:web:6f796cef324b85718fb08a',
+    measurementId: 'G-P2669M8LWR'
+  }
 };
