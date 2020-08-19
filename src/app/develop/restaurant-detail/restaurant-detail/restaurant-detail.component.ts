3fc023f14676184d7fa667b49ce5057dcf0ffeb0
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-20 00:31:13 +0900

    feat(restaurant detail): get menus

--- src/app/develop/restaurant-detail/restaurant-detail/restaurant-detail.component.ts
+++ src/app/develop/restaurant-detail/restaurant-detail/restaurant-detail.component.ts
@@ -4,6 +4,8 @@ import { RestaurantGetService } from 'src/app/services/restaurant-get.service';
 import { Restaurant } from 'src/app/interfaces/restaurant';
 import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
+import { Menu } from 'src/app/interfaces/menu';
+import { MenuGetService } from 'src/app/services/menu-get.service';
 
 @Component({
   selector: 'app-restaurant-detail',
@@ -14,14 +16,18 @@ export class RestaurantDetailComponent implements OnInit {
 
   restaurant: Observable<Restaurant>;
 
+  menus: Observable<Menu[]>;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private restaurantGetService: RestaurantGetService,
-    private location: Location
+    private location: Location,
+    private menuGetServiece: MenuGetService
   ) {
     this.activatedRoute.queryParamMap.subscribe((params) => {
       const id = params.get('id');
       this.restaurant = this.restaurantGetService.getRestaurant(id);
+      this.menus = this.menuGetServiece.getMenus(id);
     });
   }
 
--- src/app/services/menu-get.service.ts
+++ src/app/services/menu-get.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Menu } from '../interfaces/menu';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,18 @@ export class MenuGetService {
   ) { }
 
   getMenu(restaurantId: string, id: string): Observable<Menu> {
-    return this.db.doc<Menu>(`restaurant/${restaurantId}/menus/${id}`).valueChanges();
+    return this.db.doc<Menu>(`restaurants/${restaurantId}/menus/${id}`).valueChanges();
   }
 
   getMenus(restaurantId: string): Observable<Menu[]> {
-    return this.db.collection<Menu>(`restaurant/${restaurantId}/menus`).valueChanges();
+    return this.db.collection<Menu>(`restaurants/${restaurantId}/menus`).valueChanges().pipe(
+      map((menu: Menu[]) => {
+        if (menu.length) {
+          return menu;
+        } else {
+          return [];
+        }
+      })
+    );
   }
 }
