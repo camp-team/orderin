e038b71e2b0b3e97f67cc6557047e67aaab50aaa
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-19 13:42:29 +0900

    feat(restaurant): add restaurant header

--- src/app/develop/restaurant-detail/restaurant-detail.module.ts
+++ src/app/develop/restaurant-detail/restaurant-detail.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { RestaurantDetailRoutingModule } from './restaurant-detail-routing.module';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [RestaurantDetailComponent],
   imports: [
     CommonModule,
-    RestaurantDetailRoutingModule
+    RestaurantDetailRoutingModule,
+    MatChipsModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class RestaurantDetailModule { }
