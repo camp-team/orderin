60d2f5576e4722292cac9d06c8951e9e63d7fe81
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-23 01:24:22 +0900

    feat(menu card): create

--- src/app/shell/shared/shared.module.ts
+++ src/app/shell/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
 
 @NgModule({
-  declarations: [RestaurantCardComponent],
+  declarations: [RestaurantCardComponent, MenuCardComponent],
   imports: [
     CommonModule,
     SharedRoutingModule,
@@ -16,7 +17,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatIconModule
   ],
   exports: [
-    RestaurantCardComponent
+    RestaurantCardComponent,
+    MenuCardComponent
   ]
 })
 export class SharedModule { }
