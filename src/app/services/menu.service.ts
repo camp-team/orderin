1196df9a2d4b05fabe1a38e71743c46f369bf3b1
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-22 17:43:02 +0900

    feat(menu service): add updateMenu()

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -35,4 +35,34 @@ export class MenuService {
       tags: menu.tags
     });
   }
+
+  async updateMenu(menu: Menu) {
+    if (menu.imageUrl) {
+      console.log('image found');
+      const image = await this.upload(menu.restaurantId, menu.id, menu.imageUrl);
+      this.db.doc<Menu>(`restaurants/${menu.restaurantId}/menus/${menu.id}`).update({
+        id: menu.id,
+        name: menu.name,
+        imageUrl: image,
+        description: menu.description,
+        restaurantId: menu.restaurantId,
+        isSoldout: menu.isSoldout,
+        sizes: menu.sizes,
+        toppings: menu.toppings,
+        tags: menu.tags
+      });
+    } else {
+      console.log('image not found');
+      this.db.doc(`restaurants/${menu.restaurantId}/menus/${menu.id}`).update({
+        id: menu.id,
+        name: menu.name,
+        description: menu.description,
+        restaurantId: menu.restaurantId,
+        isSoldout: menu.isSoldout,
+        sizes: menu.sizes,
+        toppings: menu.toppings,
+        tags: menu.tags
+      });
+    }
+  }
 }
